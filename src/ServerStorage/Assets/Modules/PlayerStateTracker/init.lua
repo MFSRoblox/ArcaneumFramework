0d6f3ca2f1e17660f086d136f5411970e731b649
leoyulee <leoyulee.585@gmail.com>
2021-06-20 17:53:25 -0500

    Added SetState functionality

--- src/ServerStorage/Assets/Modules/PlayerStateTracker/init.lua
+++ src/ServerStorage/Assets/Modules/PlayerStateTracker/init.lua
@@ -39,6 +39,18 @@ function PlayerState:LoadState(StateData: Dictionary)
     self.Loaded = true
 end
 
+local function CheckWhitelist(Permissions: Dictionary, AllowedDataSet: Dictionary, OnAccess: Function)
+    for PermissionType, Permitted in Permissions, next do
+        if Permitted then
+            local AllowedData = AllowedDataSet[PermissionType]
+            for i=1, #AllowedData do
+                local DataName = AllowedData[i]
+                OnAccess(DataName)
+            end
+        end
+    end
+end
+
 local AllowedGet = {
     Developer = {"Player","LastAction","Class","Resources","Permissions","Equipment","Inventory","Abilities"},
     Moderator = {"Player","LastAction","Class","Resources","Permissions","Equipment","Inventory","Abilities"},
@@ -47,24 +59,17 @@ local AllowedGet = {
 }
 
 function PlayerState:GetState(Permissions:Dictionary, ...)
-    Permissions = Permissions
     local RequestedStates = {...}
     local Output = {}
     if #RequestedStates > 0 then
         if Permissions then
-            for PermissionType, Permitted in Permissions, next do
-                if Permitted then
-                    local AllowedData = AllowedGet[PermissionType]
-                    for i=1, #AllowedData do
-                        local DataName = AllowedData[i]
-                        local RequestedStatePosition = table.find(RequestedStates, DataName)
-                        if RequestedStatePosition then
-                            if Output[DataName] == nil then Output[DataName] = self[DataName] end
-                            table.remove(RequestedStates,RequestedStatePosition)
-                        end
-                    end
+            CheckWhitelist(Permissions,AllowedGet,function(DataName)
+                local RequestedStatePosition = table.find(RequestedStates, DataName)
+                if RequestedStatePosition then
+                    if Output[DataName] == nil then Output[DataName] = self[DataName] end
+                    table.remove(RequestedStates,RequestedStatePosition)
                 end
-            end
+            end)
         else
             for i=1, #RequestedStates do
                 local DataName = RequestedStates[i]
@@ -90,14 +95,30 @@ function PlayerState:GetState(Permissions:Dictionary, ...)
 end
 
 local AllowedSet = {
-    Developer = {},
+    Developer = {"Player","LastAction","Class","Resources","Permissions","Equipment","Inventory","Abilities"},
     Moderator = {},
-    CurrentPlayer = {},
+    CurrentPlayer = {"LastAction","Class","Resources","Equipment","Inventory","Abilities"},
     OtherPlayer = {},
 }
 
-function PlayerState:SetState(StateName:String, StateValue)
-    
+function PlayerState:SetState(Permissions:Dictionary, StateName:String, StateValue)
+    local StateObject = self[StateName]
+    if StateObject then
+        local function SetState(Value)
+            if StateObject["ClassName"] then
+                StateObject:SetState(Value)
+            else
+                self[StateName] = Value
+            end
+        end
+        if Permissions then
+            CheckWhitelist(Permissions,AllowedSet,function(DataName)
+                SetState(StateValue)
+            end)
+        end
+    else
+        warn(self,"was requested to set an invalid state! StateName:", StateName)
+    end
 end
 
 return PlayerState
