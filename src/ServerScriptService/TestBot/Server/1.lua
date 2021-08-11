f1b2556d73ed7b381421a5e053400869b1884544
leoyulee <leoyulee.585@gmail.com>
2021-08-11 17:24:35 -0500

    Init new test

---
+++ src/ServerScriptService/TestBot/Server/1.lua
@@ -0,0 +1,7 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ReplicatedModules = ReplicatedStorage:WaitForChild("Modules")
+local Globals = _G.Arcaneum
+local ThisTest = Globals.ClassFunctions.TestClass:New("Test 1: ServerNexus")
+--ThisTest:AddTest()
+
+return ThisTest
