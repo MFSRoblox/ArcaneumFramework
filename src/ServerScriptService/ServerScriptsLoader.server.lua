830059b9557b5d19fabeef63c59d7f05faf9059d
leoyulee <leoyulee.585@gmail.com>
2021-08-12 02:14:07 -0500

    Moved ServerNexus init to be after Global init

--- src/ServerScriptService/ServerScriptsLoader.server.lua
+++ src/ServerScriptService/ServerScriptsLoader.server.lua
@@ -1,13 +1,13 @@
 local ServerStorage = game:GetService("ServerStorage")
 local ServerAssets = ServerStorage:WaitForChild("Assets")
-local ServerModule = ServerAssets:WaitForChild("ServerModule")
-
-require(ServerModule)
+local ServerNexus = ServerAssets:WaitForChild("ServerNexus")
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ReplicatedModules = ReplicatedStorage:WaitForChild("Modules")
 local GlobalMod = ReplicatedModules:WaitForChild("Globals")
 _G.Arcaneum = require(GlobalMod)
+
+require(ServerNexus)
 
 local TestBot = script.Parent:FindFirstChild("TestBot")
 if TestBot then
