2323a2aa9abe6d6598d1d82f656b16638808e2df
leoyulee <leoyulee.585@gmail.com>
2021-08-12 02:16:43 -0500

    Added ServerHotline and more functionality

--- src/ServerStorage/Assets/ServerNexus/PlayerManager/PlayerSupervisor/init.lua
+++ src/ServerStorage/Assets/ServerNexus/PlayerManager/PlayerSupervisor/init.lua
@@ -1,5 +1,6 @@
 local Globals = _G.Arcaneum
 local BaseClass = Globals.ClassFunctions.Internal
+
 local PlayerSupervisor = BaseClass:Extend(
     {
         Version = 1;
@@ -9,8 +10,41 @@ local PlayerSupervisor = BaseClass:Extend(
 
 function PlayerSupervisor:New(Player: Player)
     local NewSupervisor = self:Extend(BaseClass:New("PlayerSupervisor","Supervisor"..Player.Name))
-
+    NewSupervisor.Player = Player
+    local PlayerInterface = Instance.new("RemoteEvent")
+    PlayerInterface.Name = "ServerHotline"
+    NewSupervisor.Interface = PlayerInterface
+    PlayerInterface.Parent = Player.PlayerScripts --Cannot be seen by other players, nice security
+    NewSupervisor.Connections.PlayerInterface = PlayerInterface.OnServerEvent:Connect(function(Sender,Data)
+        if Player ~= Sender then
+            warn(tostring(Sender).." somehow sent data from "..tostring(Player).."'s interface! Should probably look into this...")
+        end
+        NewSupervisor:DataFromPlayer(Data)
+    end)
     return NewSupervisor
+end
+
+function PlayerSupervisor:DataToPlayer(Data)
+    self.Interface:FireClient(self.Player,Data)
+end
+
+function PlayerSupervisor:DataFromPlayer(Data)
+
+end
+
+function PlayerSupervisor:LoadPlayerData()
+
+end
+
+function PlayerSupervisor:SavePlayerData()
+
+end
+
+function PlayerSupervisor:Destroy()
+    if self.Interface then
+        self.Interface:Destroy()
+        self.Interface = nil
+    end
 end
 
 return PlayerSupervisor
