f497d378faec0aed481c6b55e45a2a1edbdd322a
leoyulee <leoyulee.585@gmail.com>
2021-08-16 23:35:44 -0500

    Refactored to use BaseClass

--- src/ServerStorage/ArcaneumEngine/init.lua
+++ src/ServerStorage/ArcaneumEngine/init.lua
@@ -1,20 +1,22 @@
 local Globals = require(script.Global)
 _G.Arcaneum = Globals
-
-local Arcaneum = {
+local BaseClass = Globals.ClassFunctions.BaseClass
+local Arcaneum = BaseClass:Extend({
     Globals = Globals
-}
-Arcaneum.__index = Arcaneum
+})
 
-function Arcaneum.new()
+function Arcaneum:New()
     local Perspective = Globals.Perspective
     local PerspectiveGlobals = require(script[Perspective].Shared)
     for Name, Data in next, PerspectiveGlobals do
         if _G.Arcaneum[Name] then print("(" .. tostring(Perspective) .. ") Overwritten " .. Name .." with " .. tostring(Data)) end
         _G.Arcaneum[Name] = Data
     end
-    local self = require(script[Perspective])
-    return self
+    local Module = require(script[Perspective])
+    if Globals.IsStudio then
+        require(script.TestBot)
+    end
+    return Module
 end
 
-return Arcaneum.new()
+return Arcaneum:New()
