0accc01eb622571b8b1cd436268a16ae2b221927
leoyulee <leoyulee.585@gmail.com>
2021-08-23 01:16:50 -0500

    Created groundwork for Client testing

--- src/ServerScriptService/ServerScriptsLoader/TestBot/Tester/TestCase/init.lua
+++ src/ServerScriptService/ServerScriptsLoader/TestBot/Tester/TestCase/init.lua
@@ -3,23 +3,35 @@ local TestCaseClass = BaseClass:Extend({
     Version = 1;
     Object = script;
 })
-function TestCaseClass:New(Name: String, Function: Function, StopOnFailure: Boolean)
-    local NewTest = BaseClass:New("TestCase",Name)
-    NewTest.Function = Function or function() end;
+function TestCaseClass:New(Name: String, StopOnFailure: Boolean, StartFunction: Function)
+    local NewTest = self:Extend(BaseClass:New("TestCase",Name))
     NewTest.StopOnFailure = StopOnFailure or false;
     NewTest.Steps = {}
-    return self:Extend(NewTest)
+    if StartFunction then
+        NewTest:AddStep("Server", StartFunction)
+    end
+    return NewTest
 end
 
 function TestCaseClass:AddStep(Perspective: String, Function: Function)
-
+    table.insert(self.Steps,{
+        Perspective = Perspective;
+        Function = Function;
+    })
 end
 
 function TestCaseClass:Run()
     local initialString = "TestCase " .. self.Name
     local Success, TestResult
-    for i=1, #self.Steps do
-        Success, TestResult = pcall(self.Steps[i].Function, TestResult)
+    local Steps = self.Steps
+    for i=1, #Steps do
+        local CurrentStep = Steps[i]
+        local Perspective = CurrentStep.Perspective
+        if Perspective ~= "Client" then
+            Success, TestResult = pcall(Steps[i].Function, TestResult)
+        else
+            --Success, TestResult = --InvokeRemote Function of sorts
+        end
         if Success == false then
             break
         end
@@ -28,11 +40,11 @@ function TestCaseClass:Run()
         print(initialString.." has executed flawless as expected. Result: " .. tostring(TestResult))
         return true, TestResult
     else
-        local outputMessage = ""
+        local outputMessage = initialString
         if Success then
-            outputMessage = initialString.." didn't return a result!"
+            outputMessage ..= " didn't return a result!"
         else
-            outputMessage = initialString.." has failed to work correctly! Debug: ".. tostring(TestResult)
+            outputMessage ..= " has failed to work correctly! Debug: ".. tostring(TestResult)
         end
         if self.StopOnFailure then
             assert(false, outputMessage)
