e5c9fe192cb16e2045289784068bba2201dad312
leoyulee <leoyulee.585@gmail.com>
2021-08-23 00:04:01 -0500

    Moved TestCase

--- src/ServerScriptService/ServerScriptsLoader/TestBot/Tester/TestCase/init.lua
+++ src/ServerScriptService/ServerScriptsLoader/TestBot/Tester/TestCase/init.lua
@@ -7,12 +7,23 @@ function TestCaseClass:New(Name: String, Function: Function, StopOnFailure: Bool
     local NewTest = BaseClass:New("TestCase",Name)
     NewTest.Function = Function or function() end;
     NewTest.StopOnFailure = StopOnFailure or false;
+    NewTest.Steps = {}
     return self:Extend(NewTest)
+end
+
+function TestCaseClass:AddStep(Perspective: String, Function: Function)
+
 end
 
 function TestCaseClass:Run()
     local initialString = "TestCase " .. self.Name
-    local Success, TestResult = pcall(self.Function)
+    local Success, TestResult
+    for i=1, #self.Steps do
+        Success, TestResult = pcall(self.Steps[i].Function, TestResult)
+        if Success == false then
+            break
+        end
+    end
     if Success and TestResult ~= nil then
         print(initialString.." has executed flawless as expected. Result: " .. tostring(TestResult))
         return true, TestResult
