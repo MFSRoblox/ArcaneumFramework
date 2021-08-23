450de8c6ca4c43a1c4f7d32d8eb40ee6e1262c15
leoyulee <leoyulee.585@gmail.com>
2021-08-23 02:00:35 -0500

    Refactored for incorporating client testing

--- src/ServerScriptService/ServerScriptsLoader/TestBot/init.lua
+++ src/ServerScriptService/ServerScriptsLoader/TestBot/init.lua
@@ -1,42 +1,56 @@
 local Globals = _G.Arcaneum
 Globals.ClassFunctions.Tester = require(script.Tester)
-local TestData = {
-    Client = {};
+local RawData = {
     Server = {};
+    Client = {};
 }
 local ServerTestData = script:WaitForChild("Server",1) do
     if ServerTestData then
-        TestData.Server = ServerTestData:GetChildren()
+        RawData.Server = ServerTestData:GetChildren()
     end
 end
 local ClientTestData = script:WaitForChild("Client",1) do
     if ClientTestData then
-        TestData.Client = ClientTestData:GetChildren()
+        RawData.Client = ClientTestData:GetChildren()
     end
 end
+local NumberOfTests = #RawData.Server+#RawData.Client
+print("Number of Testers to check:",NumberOfTests)
+local TestData = {
+    Tests = table.create(NumberOfTests, nil);
+    Positions = table.create(NumberOfTests, nil)
+}
+for Perspective, DataSet in next, RawData do
+    table.sort(DataSet,function(a,b)
+        local aSuc, aNum = pcall(function() return a.Name + 0 end)
+        local bSuc, bNum = pcall(function() return b.Name + 0 end)
+        assert(aSuc and bSuc,"One of these aren't a number! A: " .. a.Name .. ", B: " .. b.Name)
+        return aNum < bNum
+    end)
+    for i=1, #DataSet do
+        local ModuleScript = DataSet[i]
+        local Position = ModuleScript.Name
+        if TestData.Tests[Position] then warn("The test with the number \""..Position.."\" [",TestData.Tests[Position],"] was replaced by", ModuleScript) end
+        TestData.Tests[Position] = {Perspective = Perspective; Tester = ModuleScript}
+        table.insert(TestData.Positions,Position)
+    end
+end
+RawData = nil
 local Separator = "----------------------------------"
 local function OnRun()
-    for DataSetName, Data in next, TestData do
-        table.sort(Data,function(a,b)
-            local aSuc, aNum = pcall(function() return a.Name + 0 end)
-            local bSuc, bNum = pcall(function() return b.Name + 0 end)
-            if aSuc and bSuc then
-                return aNum < bNum
-            else
-                warn("One of these aren't a number! A: " .. a.Name .. ", B: " .. b.Name)
-            end
-        end)
-    end
-    for _,Tester in next, TestData.Server do
-        Tester = require(Tester)
+    for i=1, #TestData.Positions do
+        local TesterData = TestData.Tests[TestData.Positions[i]]
+        local Perspective = TesterData.Perspective
+        local Module = TesterData.Tester
+        local Tester = require(Module)
         local DisplayName = Tester.DisplayName
         local TestName = Tester.Name
-        if Tester.RunTests then
+        if Perspective == "Server" and Tester.RunTests then
             print("\n\n"..Separator.."\n" .. DisplayName .. " will now start their tests (".. TestName ..").")
             local TesterFeedback = Tester:RunTests()
             print("\n\n"..DisplayName.." has finished their tests! Here's their report:")
-            for i=1, #TesterFeedback do
-                local Result = TesterFeedback[i]
+            for j=1, #TesterFeedback do
+                local Result = TesterFeedback[j]
                 if Result.IsSuccessful then
                     print(Result)
                 else
