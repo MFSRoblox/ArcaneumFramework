55dd8f1dbde998bfebdfb712875f16df07f81fe0
leoyulee <leoyulee.585@gmail.com>
2021-08-23 01:29:50 -0500

    Added debug print for server startup

--- src/ServerScriptService/ServerScriptsLoader/init.server.lua
+++ src/ServerScriptService/ServerScriptsLoader/init.server.lua
@@ -1,7 +1,8 @@
 local ServerStorage = game:GetService("ServerStorage")
 local BootDrive = ServerStorage:WaitForChild("ArcaneumEngine")
+print("Booting")
 require(BootDrive)
-
+print("Booted")
 if _G.Arcaneum.IsTesting then
     require(script.TestBot)
 end
