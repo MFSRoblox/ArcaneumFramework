3a20d93d8cfdec1909c9b6a06497519cf3574719
Leo Yu Lee <leoyulee.585@gmail.com>
2021-09-03 15:07:34 -0500

    Established Test

--- src/ServerScriptService/ServerScriptsLoader/TestBot/Server/2.lua
+++ src/ServerScriptService/ServerScriptsLoader/TestBot/Server/2.lua
@@ -2,18 +2,13 @@ local Globals = _G.Arcaneum
 local TargetPlayer = Globals.TestBot.TestPlayer
 print("Got TargetPlayer:",TargetPlayer)
 local ThisTest = Globals.ClassFunctions.Tester:New("Client Foundations")
-
-local ProxyFunction
-local ProxyEvent
-local Timeout = 10
-local EventFunctions = {
-
-}
 ThisTest:AddTest("TestBotProxy Check", true, "Client")
 local ConnectionTest = ThisTest:AddTest("Client Connection Test", true, "Client")
-ConnectionTest:AddStep("Client",function(HasConnection)
+local ClientConnector = ConnectionTest.ClientConnector
+ConnectionTest:AddStep("[2]ClientResponseTest",function(HasConnection)
     assert(HasConnection,"Stopped test due to lack of connection.")
-    print("insert test stuff here")
-    return true
+    return {
+        Message = "Hello World!";
+    }
 end)
 return ThisTest
