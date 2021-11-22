e37a0cc8018f88393505c965793111725fc6d37f
leoyulee <leoyulee.585@gmail.com>
2021-11-21 22:24:00 -0600

    Added initialization text

--- src/ServerStorage/ArcaneumEngine/init.lua
+++ src/ServerStorage/ArcaneumEngine/init.lua
@@ -1,6 +1,8 @@
+print("Initializing Arcaneum Globals...")
 local Globals = require(script.Global)
 _G.Arcaneum = Globals
 Globals.ClassFunctions.ClientConnector = Globals.Utilities:ImportModule(script,"Global","ClassFunctions","Class","Internal","ClientConnector")
+print("Arcaneum Globals:",Globals)
 local BaseClass = Globals.ClassFunctions.Class
 local Arcaneum = BaseClass:Extend({
     Globals = Globals
@@ -8,7 +10,9 @@ local Arcaneum = BaseClass:Extend({
 
 function Arcaneum:New()
     local Perspective = Globals.Perspective
+    print("Arcaneum initializing on",Perspective)
     local PerspectiveGlobals = require(script[Perspective].Shared)
+    print("Arcaneum",Perspective,"Globals:",PerspectiveGlobals)
     for Name, Data in next, PerspectiveGlobals do
         if _G.Arcaneum[Name] then print("(" .. tostring(Perspective) .. ") Overwritten " .. Name .." with " .. tostring(Data)) end
         _G.Arcaneum[Name] = Data
