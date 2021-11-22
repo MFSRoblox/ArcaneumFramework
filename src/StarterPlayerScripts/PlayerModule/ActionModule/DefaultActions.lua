43a7fe619b575f9f3714692a9eb4e3f7e8ac666d
leoyulee <leoyulee.585@gmail.com>
2021-11-21 20:54:09 -0600

    Removed unnessessary files

--- src/StarterPlayerScripts/PlayerModule/ActionModule/DefaultActions.lua
+++
@@ -1,7 +0,0 @@
-local DefaultActions = {
-    attack = {
-        punch = "http://www.roblox.com/asset/?id=6745003570";
-    }
-}
-
-return DefaultActions
--- src/StarterPlayerScripts/PlayerModule/ReactionModule/DefaultActions.lua
+++
@@ -1,5 +0,0 @@
-local DefaultActions = {
-    punch = "http://www.roblox.com/asset/?id=6745003570";
-}
-
-return DefaultActions
