f7f42069358b5d340d8542914a503c37a0570835
leoyulee <leoyulee.585@gmail.com>
2021-11-21 22:39:35 -0600

    Increased wait time for complex testing

--- src/ServerScriptService/ServerScriptsLoader/init.server.lua
+++ src/ServerScriptService/ServerScriptsLoader/init.server.lua
@@ -8,7 +8,7 @@ print("Booted")
 if _G.Arcaneum.IsStudio and not RunService:IsRunMode() then
     game:GetService("Players").PlayerAdded:Wait()]]
 --end
-local WaitTime = 5
+local WaitTime = 10
 if _G.Arcaneum.IsTesting then
     print("Waiting a little in case a player is being added...\n Starting tests in:")
     for i=WaitTime, 1, -1 do
