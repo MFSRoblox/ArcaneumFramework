66209e7c6179b161d8848301d8479b001a8cd4e8
Leo Yu Lee <leoyulee.585@gmail.com>
2021-11-29 19:30:33 -0600

    Added Moonwave documentation

--- src/ServerStorage/ArcaneumEngine/Global/ClassFunctions/Class/init.lua
+++ src/ServerStorage/ArcaneumEngine/Global/ClassFunctions/Class/init.lua
@@ -1,7 +1,25 @@
+--[=[
+    @class BaseClass
+    @server
+    @client
+    The foundational class for all classes.
+]=]
 local BaseClass = {
     Version = 0;
     Object = script;
 }
+--[=[
+    @prop Object script
+    @within BaseClass
+    The script itself for external reference.
+]=]
+
+--[=[
+    This is a very fancy function that sets the index of the table so you don't need to repeatedly .__index and setmetatable.
+
+    @param NewObject table -- The table you want to set the __index metatable to.
+    @return table -- Returns the table with the index set to itself.
+]=]
 function BaseClass:Extend(NewObject)
     NewObject = NewObject or {
         ClassName = "";
@@ -11,11 +29,29 @@ function BaseClass:Extend(NewObject)
     local output = setmetatable(NewObject, self)
     return output
 end
+--[=[
+    Creates a raw BaseClass.
 
+    @param ClassName string -- The name of the class being created.
+    @return NewBaseClass -- Returns an object with the ClassName of "ClassName".
+]=]
+--[=[
+    @prop ClassName string
+    @within BaseClass
+    The name of the object's class.
+]=]
+--[=[
+    @prop Connections table
+    @within BaseClass
+    A table containing all existing connections to this object.
+]=]
 function BaseClass:New(ClassName:string)
     return self:Extend({ClassName = ClassName; Connections = {};})
 end
 
+--[=[
+    Destroy the BaseClass Object to clear up memory.
+]=]
 function BaseClass:Destroy(): nil
     for Label, Connection in next, self.Connections do
         Connection:Disconnect()
