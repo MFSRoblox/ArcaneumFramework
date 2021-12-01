b030948f0ba6f999e8da21c91ea9432dde7b78ee
leoyulee <leoyulee.585@gmail.com>
2021-12-01 04:50:07 -0600

    Replaced _G with ArcaneumGlobals

--- src/ServerScriptService/ServerScriptsLoader/init.server.lua
+++ src/ServerScriptService/ServerScriptsLoader/init.server.lua
@@ -3,6 +3,16 @@ local BootDrive = ServerStorage:WaitForChild("ArcaneumEngine")
 print("Booting")
 local Software = require(BootDrive)
 print("Booted")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local GlobalModuleName = "Arcaneum"
+local ArcaneumGlobals repeat
+    ArcaneumGlobals = ReplicatedStorage:FindFirstChild(GlobalModuleName)
+    if ArcaneumGlobals == nil then
+        task.wait(1)
+    else
+        ArcaneumGlobals = require(ArcaneumGlobals)
+    end
+until ArcaneumGlobals ~= nil
 --[[
 local RunService = game:GetService("RunService")
 print(RunService:IsRunMode())
@@ -10,7 +20,8 @@ if _G.Arcaneum.IsStudio and not RunService:IsRunMode() then
     game:GetService("Players").PlayerAdded:Wait()]]
 --end
 local WaitTime = 10
-if _G.Arcaneum.IsTesting then
+
+if ArcaneumGlobals.IsTesting then
     print("Waiting a little in case a player is being added...\n Starting tests in:")
     for i=WaitTime, 1, -1 do
         print(i)
