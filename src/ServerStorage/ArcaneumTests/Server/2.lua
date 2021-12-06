8ecf678e8db9f0ebb497356f17bfcab03cb28a9c
Leo Yu Lee <leoyulee.585@gmail.com>
2021-12-06 12:10:20 -0600

    Made tests functional

--- src/ServerStorage/ArcaneumTests/Server/2.lua
+++ src/ServerStorage/ArcaneumTests/Server/2.lua
@@ -1,23 +1,19 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local GlobalModuleName = "Arcaneum"
-local ArcaneumGlobals repeat
-    ArcaneumGlobals = ReplicatedStorage:FindFirstChild(GlobalModuleName)
-    if ArcaneumGlobals == nil then
-        task.wait(1)
-    else
-        ArcaneumGlobals = require(ArcaneumGlobals)
+return function(self)
+    --local ArcaneumGlobals = self.ArcaneumGlobals
+    local TargetPlayer = self.TestPlayer
+    if TargetPlayer == nil then
+        return {}
     end
-until ArcaneumGlobals ~= nil
-local TargetPlayer = ArcaneumGlobals.TestBot.TestPlayer
-print("Got TargetPlayer:",TargetPlayer)
-local ThisTest = ArcaneumGlobals.ClassFunctions.Tester:New("Client Foundations")
-ThisTest:AddTest("TestBotProxy Check", true, "Client")
-local ConnectionTest = ThisTest:AddTest("Client Connection Test", true, "Client")
-local ClientConnector = ConnectionTest.ClientConnector
-ConnectionTest:AddStep("[2]ClientResponseTest",function(HasConnection)
-    assert(HasConnection,"Stopped test due to lack of connection.")
-    return {
-        Message = "Hello World!";
-    }
-end)
-return ThisTest
+    print("Got TargetPlayer:",TargetPlayer)
+    local ThisTest = self.TesterClass:New("Client Foundations")
+    ThisTest:AddTest("TestBotProxy Check", true, "Client")
+    local ConnectionTest = ThisTest:AddTest("Client Connection Test", true, "Client")
+    local ClientConnector = ConnectionTest.ClientConnector
+    ConnectionTest:AddStep("[2]ClientResponseTest",function(HasConnection)
+        assert(HasConnection,"Stopped test due to lack of connection.")
+        return {
+            Message = "Hello World!";
+        }
+    end)
+    return ThisTest
+end
