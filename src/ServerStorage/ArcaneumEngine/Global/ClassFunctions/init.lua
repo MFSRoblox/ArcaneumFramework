02b00f951f9b470c2a254548548e04e7f2d2660d
Leo Yu Lee <leoyulee.585@gmail.com>
2021-12-15 13:04:50 -0600

    Modified initialization process

--- src/ServerStorage/ArcaneumEngine/Global/ClassFunctions/init.lua
+++ src/ServerStorage/ArcaneumEngine/Global/ClassFunctions/init.lua
@@ -1,7 +1,9 @@
-local ClassService = {}
+local BaseClass = require(script.BaseClass)
+local ClassService = BaseClass:New("ClassService")
 function ClassService:AddClass(ClassName: string, ClassData: table)
     self[ClassName] = ClassData
 end
+ClassService:AddClass("BaseClass",BaseClass)
 function ClassService:GetClass(ClassName: string): any
     local RequestedClass = self[ClassName]
     if RequestedClass ~= nil then
@@ -10,10 +12,18 @@ function ClassService:GetClass(ClassName: string): any
         warn("ClassService was asked to return a class that doesn't exist!",ClassName,debug.traceback())
     end
 end
-function ClassService.Setup(_output: table, ArcaneumGlobals: table): table
-    local Utilities = ArcaneumGlobals.Utilities
-    local BaseClass = Utilities:ImportModule(script,"BaseClass")
-    ClassService = BaseClass:NewFromTable(ClassService,"ClassService")
+local InitialModules = {
+    script.BaseClass.Class;
+    script.BaseClass.DataTypes;
+    table.unpack(script.BaseClass.DataTypes:GetChildren());
+    script.InitializerService;
+    script.InitializerService.SingletonInitClass;
+}
+for i=1, #InitialModules do
+    local Module = InitialModules[i]
+    ClassService:AddClass(Module.Name, require(Module))
+end
+function ClassService.Setup(_output: table, _ArcaneumGlobals: table): table
     local function UnpackClasses(Parent: ModuleScript): table
         local PotentialModules = Parent:GetChildren()
         for i=1, #PotentialModules do
