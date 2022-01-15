6c42bdc53682b863098b75d631c9dc0bde4e5b8c
Leo Yu Lee <leoyulee.585@gmail.com>
2022-01-14 23:00:42 -0600

    Added Intercept Accelerating Object Check

--- src/ServerStorage/ArcaneumTests/Server/3.lua
+++ src/ServerStorage/ArcaneumTests/Server/3.lua
@@ -42,5 +42,39 @@ return function(self)
         --assert(SimulatedHitPosition - PrecisionVector <= ProjectedHitPosition and ProjectedHitPosition <= SimulatedHitPosition + PrecisionVector, "Target Intercept and Simulated Hit don't equal each other!")
         return true
     end)
+    ThisTest:AddTest("Intercept Accelerating Object Check", false, function()
+        local ProjecitleSpeed = 100
+        local ShooterPosition = Vector3.new()
+        local TargetPosition = Vector3.new(0,0,100)
+        local TargetVelocity = Vector3.new(0,0,1)
+        local TargetAcceleration = Vector3.new(0,0,1)
+        local results = table.pack(BalisticsFunctions:GetTargetTimes(ProjecitleSpeed, ShooterPosition, nil, nil, TargetPosition, TargetVelocity, TargetAcceleration))
+        print("Hitable Check Results:", table.unpack(results))
+        assert(#results > 0, "Hitable Check failure! Not enough numbers were returned!")
+        table.sort(results,function(a,b)
+            if a >= 0 then
+                if b >= 0 then
+                    return a < b
+                else
+                    return true
+                end
+            else
+                return false
+            end
+        end)
+        local MinimalTime = results[1]
+        --MinimalTime = tonumber(string.format(PrecisionString,MinimalTime))
+        print("Minimal Time to hit the target:",MinimalTime)
+        assert(MinimalTime ~= nil and MinimalTime >= 0, "Minimal Time is negative or doesn't exist!")
+        local ProjectedHitPosition = TargetPosition + TargetVelocity * MinimalTime + 0.5 * TargetAcceleration * MinimalTime*MinimalTime
+        print("Target Intercept Position:", ProjectedHitPosition)
+        local SimulationLookVector = (ProjectedHitPosition - ShooterPosition).Unit
+        print("Simulated Look Vector to hit target:", SimulationLookVector)
+        local SimulatedHitPosition = ShooterPosition + SimulationLookVector * ProjecitleSpeed * MinimalTime
+        print("Simulated Hit Position:", SimulatedHitPosition)
+        assert(CompareVectors(SimulatedHitPosition,ProjectedHitPosition),"Target Intercept and Simulated Hit don't equal each other!")
+        --assert(SimulatedHitPosition - PrecisionVector <= ProjectedHitPosition and ProjectedHitPosition <= SimulatedHitPosition + PrecisionVector, "Target Intercept and Simulated Hit don't equal each other!")
+        return true
+    end)
     return ThisTest
 end
