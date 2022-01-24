bb40a22a1f1d4978cd0a56416aec743d7fbaeaf4
Leo Yu Lee <leoyulee.585@gmail.com>
2022-01-23 20:22:19 -0600

    Refactored Code

--- src/ServerStorage/ArcaneumTests/Server/3.lua
+++ src/ServerStorage/ArcaneumTests/Server/3.lua
@@ -1,23 +1,22 @@
 return function(self)
     local ArcaneumGlobals = self.ArcaneumGlobals
-    local BalisticsFunctions = ArcaneumGlobals.Ballistics
-    local DesiredPrecision = 1
+    local Ballistics = ArcaneumGlobals.Ballistics
+    local Utilities = Ballistics.Utilities
+    local DesiredPrecision = 2
     local PrecisionVector = Vector3.new(1,1,1)*DesiredPrecision
+    local function CompareNumbers(N1: number, N2: number, Precision: number?)
+        return Utilities:CompareNumbers(N1, N2, Precision)
+    end
     local function CompareVectors(V1: Vector3, V2: Vector3)
         if V1 == V2 then
             return true
         end
-        local V1L = V1 - PrecisionVector
-        local V1G = V1 + PrecisionVector
+        local V1L = V1 - PrecisionVector/2
+        local V1G = V1 + PrecisionVector/2
         return (V1L.X <= V2.X and V2.X <= V1G.X) and (V1L.Y <= V2.Y and V2.Y <= V1G.Y) and (V1L.Z <= V2.Z and V2.Z <= V1G.Z)
     end
-    local function CompareNumbers(N1: number, N2: number)
-        if N1 == N2 then
-            return true
-        end
-        local N1L = N1 - DesiredPrecision
-        local N1G = N1 + DesiredPrecision
-        return (N1L <= N2 and N2 <= N1G)
+    local function RemoveDuplicatesFromTable(InputTable: table)
+        return Utilities:RemoveDuplicatesFromTable(InputTable)
     end
     local ThisTest = self.TesterClass:New("Balistics Functions")
     ThisTest:AddTest("Simple Linear Hitable Check", false, function()
@@ -26,7 +25,7 @@ return function(self)
         local TargetPosition = Vector3.new(0,0,100)
         local TargetVelocity = Vector3.new(0,0,1)
         print("Hit a target with a projectile that moves",ProjecitleSpeed,"studs per second, where the target is at",TargetPosition,"\nwith a velocity of",TargetVelocity,"studs per second")
-        local results = table.pack(BalisticsFunctions:GetHitTimes(ProjecitleSpeed, ShooterPosition, nil, nil, TargetPosition, TargetVelocity))
+        local results = table.pack(Ballistics:GetHitTimes(ProjecitleSpeed, ShooterPosition, nil, nil, TargetPosition, TargetVelocity))
         print("Hitable Check Results:", table.unpack(results))
         assert(#results > 0, "Hitable Check failure! Not enough numbers were returned!")
         table.sort(results,function(a,b)
@@ -84,29 +83,19 @@ return function(self)
             Solutions = {1}
         };
     }
-    local function RemoveDuplicatesFromTable(InputTable: table)
-        local output = {}
-        for i=1, #InputTable do
-            local v = InputTable[i]
-            if not table.find(output,v) then
-                table.insert(output,v)
-            end
-        end
-        return output
-    end
     ThisTest:AddTest("Cubic Polynomial Check", true, function()
         for DataNumber,TestData in pairs(CubicTestData) do
             local Coefficients = TestData.Coefficients
             local Solutions = TestData.Solutions
             print(string.format("Find the roots of f(x) = %d + %dx + %dx^2 + %dx^3.",table.unpack(Coefficients)))
-            local GeneratedSolutions = RemoveDuplicatesFromTable(table.pack(BalisticsFunctions:SolvePolynomial(table.unpack(Coefficients))))
+            local GeneratedSolutions = RemoveDuplicatesFromTable(table.pack(Utilities:SolvePolynomial(table.unpack(Coefficients))))
             table.sort(Solutions)
             table.sort(GeneratedSolutions)
             print("Generated Solutions:",table.unpack(GeneratedSolutions))
             print("Actual Solutions:",table.unpack(Solutions))
             assert(#Solutions == #GeneratedSolutions, "Number of Generated Solutions don't match Actual Solutions of Test " .. tostring(DataNumber) .."!")
             for i=1, #Solutions do
-                assert(CompareNumbers(GeneratedSolutions[i],Solutions[i]), "Generated Solutions don't match Actual Solutions of Test " .. tostring(DataNumber) .."!")
+                assert(CompareNumbers(GeneratedSolutions[i],Solutions[i],DesiredPrecision), "Generated Solutions don't match Actual Solutions of Test " .. tostring(DataNumber) .."!")
             end
         end
         return true
@@ -118,7 +107,7 @@ return function(self)
         local TargetVelocity = Vector3.new(0,0,1)
         local TargetAcceleration = Vector3.new(0,0,1)
         print("Hit a target with a projectile that moves",ProjecitleSpeed,"studs per second, where the target is at",TargetPosition,"\nwith a velocity of",TargetVelocity,"studs per second \nand an acceleration of",TargetAcceleration,"studs per second per second")
-        local results = table.pack(BalisticsFunctions:GetHitTimes(ProjecitleSpeed, ShooterPosition, nil, nil, TargetPosition, TargetVelocity, TargetAcceleration))
+        local results = table.pack(Ballistics:GetHitTimes(ProjecitleSpeed, ShooterPosition, nil, nil, TargetPosition, TargetVelocity, TargetAcceleration))
         print("Hitable Check Results:", table.unpack(results))
         print("What it should be close to:", 1.01531, 196.98469)
         assert(#results > 0, "Hitable Check failure! Not enough numbers were returned!")
@@ -155,7 +144,7 @@ return function(self)
         local TargetAcceleration = Vector3.new(0,0,1)
         local TargetJerk = Vector3.new(0,0,1)
         print("Hit a target with a projectile that moves",ProjecitleSpeed,"studs per second, where the target is at",TargetPosition,"\nwith a velocity of",TargetVelocity,"studs per second \nand an acceleration of",TargetAcceleration,"studs per second per second \nand jerk of",TargetJerk)
-        local results = table.pack(BalisticsFunctions:GetHitTimesWithJerk(ProjecitleSpeed, ShooterPosition, nil, nil, nil, TargetPosition, TargetVelocity, TargetAcceleration, TargetJerk))
+        local results = table.pack(Ballistics:GetHitTimesWithJerk(ProjecitleSpeed, ShooterPosition, nil, nil, nil, TargetPosition, TargetVelocity, TargetAcceleration, TargetJerk))
         print("Hitable Check Results:", table.unpack(results))
         print("What it should be close to:", 1.28)-- 12.797, 36.475
         assert(#results > 0, "Hitable Check failure! Not enough numbers were returned!")
@@ -192,7 +181,7 @@ return function(self)
         local TargetAcceleration = Vector3.new(0,0,1)
         local TargetJerk = Vector3.new(0,0,1)
         print("Hit a target with a projectile that moves",ProjecitleSpeed,"studs per second, where the target is at",TargetPosition,"\nwith a velocity of",TargetVelocity,"studs per second \nand an acceleration of",TargetAcceleration,"studs per second per second \nand jerk of",TargetJerk)
-        local results = table.pack(BalisticsFunctions:GetHitTimesWithJerk(ProjecitleSpeed, ShooterPosition, nil, nil, nil, TargetPosition, TargetVelocity, TargetAcceleration, TargetJerk))
+        local results = table.pack(Ballistics:GetHitTimesWithJerk(ProjecitleSpeed, ShooterPosition, nil, nil, nil, TargetPosition, TargetVelocity, TargetAcceleration, TargetJerk))
         print("Hitable Check Results:", table.unpack(results))
         print("What it should be close to:", 1.019)
         assert(#results > 0, "Hitable Check failure! Not enough numbers were returned!")
@@ -229,7 +218,7 @@ return function(self)
         local TargetAcceleration = Vector3.new(20,3,1)
         local TargetJerk = Vector3.new(-1,5,1)
         print("Hit a target with a projectile that moves",ProjecitleSpeed,"studs per second, where the target is at",TargetPosition,"\nwith a velocity of",TargetVelocity,"studs per second \nand an acceleration of",TargetAcceleration,"studs per second per second \nand jerk of",TargetJerk)
-        local results = table.pack(BalisticsFunctions:GetHitTimesWithJerk(ProjecitleSpeed, ShooterPosition, nil, nil, nil, TargetPosition, TargetVelocity, TargetAcceleration, TargetJerk))
+        local results = table.pack(Ballistics:GetHitTimesWithJerk(ProjecitleSpeed, ShooterPosition, nil, nil, nil, TargetPosition, TargetVelocity, TargetAcceleration, TargetJerk))
         print("Hitable Check Results:", table.unpack(results))
         --print("What it should be close to:", 1.019)
         assert(#results > 0, "Hitable Check failure! Not enough numbers were returned!")
