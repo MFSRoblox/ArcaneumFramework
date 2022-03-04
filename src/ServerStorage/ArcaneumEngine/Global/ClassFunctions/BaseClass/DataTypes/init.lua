f3a0a3ae36047355712c05cf3959900ce7439372
Leo Yu Lee <leoyulee.585@gmail.com>
2022-03-03 19:48:39 -0600

    Added documentation and interface

--- src/ServerStorage/ArcaneumEngine/Global/ClassFunctions/BaseClass/DataTypes/init.lua
+++ src/ServerStorage/ArcaneumEngine/Global/ClassFunctions/BaseClass/DataTypes/init.lua
@@ -1,10 +1,45 @@
 local BaseClass = require(script.Parent)
 local DataTypeInterface = BaseClass:New("DataTypeInterface")
-function DataTypeInterface.new()
-    assert(false, "DataType didn't set a .new() function!" .. debug.traceback())
-end
+--[=[
+    @class DataTypes
+    @client
+    @server
+
+    The parent class for all DataTypes to initalize off. Sets commonly used metamethods and methods to throw errors.
+]=]
+--[=[
+    @interface DataType
+    @within DataTypes
+
+    .new (...) -> (self)
+    .__tostring (self) -> (string)
+    .__concat (any,any) -> (string)
+    .__eq (self,any) -> (boolean)
+    .lt (self,any) -> (boolean)
+    .le (self,any) -> (boolean)
+]=]
+
 function DataTypeInterface:NewClass(ClassName: string)
     local NewClass = self:Extend(BaseClass:New(ClassName))
     return NewClass
 end
+
+function DataTypeInterface.new()
+    error("DataType didn't set a .new() function!")-- .. debug.traceback())
+end
+DataTypeInterface.__tostring = function(_self)
+    error("Attempt to convert unset datatype to string.")
+end;
+DataTypeInterface.__concat = function(value1: any,value2: any)
+    return string.format("%s%s",tostring(value1),tostring(value2))
+end;
+DataTypeInterface.__eq = function(_self,_value)
+    error("Attempt to compare unset datatype another object.")
+end;
+DataTypeInterface.__lt = function(_self, _value)
+    error("Attempt to compare unset datatype another object.")
+end;
+DataTypeInterface.__le = function(_self, _value)
+    error("Attempt to compare unset datatype another object.")
+end;
 return DataTypeInterface
