2b2b801f8c5cf75e38d99554dec6471e3f448dad
Leo Yu Lee <leoyulee.585@gmail.com>
2022-03-03 21:06:34 -0600

    Added debug text

--- src/ServerStorage/ArcaneumTests/Server/0.lua
+++ src/ServerStorage/ArcaneumTests/Server/0.lua
@@ -74,27 +74,35 @@ return function(self)
     ThisTest:AddTest("Version Test", true, function()
         --init test
         local Version = ArcaneumGlobals.ClassFunctions:GetClass("Version")
+        print("Checking Version Class")
         assert(Version, "Version didn't return anything!")
         assert(type(Version) == "table", "Version isn't a table!")
         assert(type(Version.new) == "function", "Version.new isn't a function!")
         --.new test
+        print("Checking Version Creation")
         local Version1 = Version.new(1,2,3)
+        print("Version1 Metatable:",getmetatable(Version1))
         assert(Version1:GetMajorVersion() == 1, "Test Version1 Object did not set its MajorVersion properly! Expected 1, got "..Version1:GetMajorVersion())
         assert(Version1:GetMinorVersion() == 2, "Test Version1 Object did not set its MinorVersion properly! Expected 2, got "..Version1:GetMinorVersion())
         assert(Version1:GetPatchVersion() == 3, "Test Version1 Object did not set its PatchVersion properly! Expected 3, got "..Version1:GetPatchVersion())
         --__tostring test
+        print("Version1:__tostring Test")
         assert(tostring(Version1) == "1.2.3", "Test Version1 Object did not return expected result of 1.2.3! Instead, it returned: "..tostring(Version1))
         --__lt and __le tests
+        print("Version1:__lt and __le Test")
         local MajorLessVersion = Version.new(0,4,5)
+        --print("MajorLessVersion Metatable:",getmetatable(MajorLessVersion))
         assert(getmetatable(Version1) == getmetatable(MajorLessVersion),"Test Version1 Object and MajorLessVersion Object should share the same metatable!")
         assert(Version1 > MajorLessVersion, "Test Version1 Object should be greater than MajorLessVersion Object!")
         assert(Version1 >= MajorLessVersion, "Test Version1 Object should be greater than MajorLessVersion Object!")
         assert(not (Version1 < MajorLessVersion), "Test Version1 Object should be greater than MajorLessVersion Object!")
         assert(not (Version1 <= MajorLessVersion), "Test Version1 Object should be greater than MajorLessVersion Object!")
         --concat test
+        print("Version:__concat Test")
         local ConcatTest = "Version1: "..Version1
         assert(ConcatTest == "Version1: 1.2.3", "ConcatTEst with Version1: .. Version1 did not return Version1: 1.2.3! Instead it returned: ".. ConcatTest)
         --__eq test
+        print("Version:__eq Test")
         local DupeVersion1 = Version.new(1,2,3)
         assert(Version1 == DupeVersion1, "Version1 and DupeVersion1 should be equivelant, but isn't!")
         return true
