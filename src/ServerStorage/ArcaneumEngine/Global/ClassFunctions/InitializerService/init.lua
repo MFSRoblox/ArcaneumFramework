6070a69e79e8ffe29b0c1d4dad7141ea6e045b39
Leo Yu Lee <leoyulee.585@gmail.com>
2022-03-17 19:07:28 -0500

    Simplified process

--- src/ServerStorage/ArcaneumEngine/Global/ClassFunctions/InitializerService/init.lua
+++ src/ServerStorage/ArcaneumEngine/Global/ClassFunctions/InitializerService/init.lua
@@ -16,8 +16,7 @@ type FilesToBoot = {[FileName]: Array<SingletonInitClass>}
 type FilesWillBoot = {}
 type BootOrder = number
 type BootGroup = Array<SingletonInitClass>
-type BootGroups = {[BootOrder]: BootGroup}
-type InitializerProperties = {FilesToBoot:FilesToBoot, FilesWillBoot:FilesWillBoot, BootGroups:BootGroups}
+type InitializerProperties = {FilesToBoot:FilesToBoot}
 type InitializerObject = typeof(InitializerService:New()) & InitializerProperties
 local DataTypesFolder = script.Parent.BaseClass.DataTypes
 local VersionClass = require(DataTypesFolder.Version)
@@ -45,7 +44,7 @@ function InitializerService:InitializeModule(ModuleScript: ModuleScript, Globals
     return TempInitializer:InitializeAll(Globals)[ModuleScript.Name]
 end
 --[=[
-    @type InitializerObject {FilesToBoot: {[FileName]: Array<SingletonInitClass>}, FilesWillBoot: {}, BootGroups: {[BootOrder]: BootGroup},}
+    @type InitializerObject {FilesToBoot: {[FileName]: Array<SingletonInitClass>}}
     @within InitializerService
 
     The object created by [InitializerService:New] to initialize a batch of modules
@@ -57,8 +56,6 @@ end
 function InitializerService:New(): InitializerObject
     local NewService = setmetatable({
         FilesToBoot = {} :: {[FileName]: Array<SingletonInitClass>}; --A dictionary
-        FilesWillBoot = {};
-        BootGroups = {} :: {[BootOrder]: BootGroup};
         Output = {};
     },{__index = self})
     return NewService
@@ -113,38 +110,43 @@ end
 ]=]
 function InitializerService:InitializeAll(Globals: table?): {[string]: any}
     self:CheckDependacies(Globals)
-    local BootGroups:BootGroups = self.BootGroups --Get current BootGroups dictionary
-    local BootOrders: Array<BootOrder> = {} do --Grab all of the BootOrder keys from BootGroups and sort them
-        for BootOrder, _ in next, BootGroups do
-            local PreviousOrder = table.find(BootOrders,BootOrder)
-            if PreviousOrder == nil then
-                table.insert(BootOrders, BootOrder)
-            end
+    local FilesToBoot = self.FilesToBoot
+    print("Ordering FilesToBoot:",FilesToBoot)
+    local SortedFilesToBoot = {} :: Array<SingletonInitClass> do
+        for _FileName, InitClasses in next, FilesToBoot do
+            print("Inserting",InitClasses[1],"to SortedFilesToBoot...")
+            table.insert(SortedFilesToBoot, InitClasses[1])
         end
-        table.sort(BootOrders,function(OrderA: number,OrderB: number)
-            return OrderA < OrderB
-        end)
     end
+    table.sort(SortedFilesToBoot, function(File1:SingletonInitClass,File2:SingletonInitClass)
+        if File1 ~= nil then
+            if File2 ~= nil then
+                return File1.BootOrder < File2.BootOrder
+            else
+                return true
+            end
+        else
+            return false
+        end
+    end)
+    print("Booting SortedFilesToBoot:",SortedFilesToBoot)
     local Output = self.Output
     local InitializedModules = self.InitializedModules
-    for i=1, #BootOrders do
-        local CurrentOrder = BootOrders[i]
-        print("Initializing BootGroup",i)
-        local CurrentGroup = BootGroups[CurrentOrder]
-        for j=1, #CurrentGroup do
-            local FileToBoot = CurrentGroup[j]
-            local FileName = FileToBoot.InitName
-            print("Initialing",FileName)
-            if Output[FileName] ~= nil then
-                warn(FileName,"was found in Boot! Potential Output override!")
-            end
-            Output[FileName] = FileToBoot(Globals)
-            if InitializedModules[FileName] ~= nil then
-                warn(FileName,"was found in InitializedModules! Potential InitializedModules override!")
-            end
-            InitializedModules[FileName] = {tostring(FileToBoot.Version)}
+    for i=1, #SortedFilesToBoot do
+        local FileToBoot = SortedFilesToBoot[i]
+        local FileName = FileToBoot.InitName
+        print("Initialing",FileName)
+        if Output[FileName] ~= nil then
+            warn(FileName,"was found in Boot! Potential Output override!")
         end
+        Output[FileName] = FileToBoot(Globals)
+        if InitializedModules[FileName] ~= nil then
+            warn(FileName,"was found in InitializedModules! Potential InitializedModules override!")
+        end
+        InitializedModules[FileName] = {tostring(FileToBoot.Version)}
     end
+    print("Done! Output:",Output)
+    print("Cleaning up...")
     self:Destroy()
     return Output
 end
@@ -156,10 +158,9 @@ end
 ]=]
 function InitializerService:CheckDependacies(_Globals: table?)
     type FileNameVersion = string
-    local Dependacies:{[FileNameVersion]: {Object: DependacyObject; Dependants: Array<SingletonInitClass>}} = {}
-    --[=[
-        Generally, if a module needs another to function, the other file needs to be initialized first. Thus, if we track the modules that are depended upon, then we can sort the order they should launch in.
-    ]=]
+    local Dependacies:{[FileNameVersion]: {Object: DependacyObject; InitObj: SingletonInitClass; Dependants: Array<SingletonInitClass>}} = {}
+    local DependacyOrder: Array<FileNameVersion> = {}
+    --Generally, if a module needs another to function, the other file needs to be initialized first. Thus, if we track the modules that are depended upon, then we can sort the order they should launch in.
     local FilesToBoot: FilesToBoot = self.FilesToBoot
     for FileName, FileInits in next, FilesToBoot do --initial setup and add to group, get dependacies
         assert(type(FileInits) == "table", "FileContents of " .. FileName .." is not a table! " .. debug.traceback())
@@ -182,10 +183,23 @@ function InitializerService:CheckDependacies(_Globals: table?)
         local ThisFileDependacies: Array<DependacyObject> = FileToBoot.Dependacies
         for _, Dependacy in next, ThisFileDependacies do
             local DependacyKey = tostring(Dependacy)
+            local DependacyInitObj do
+                local FileInitObjs = FilesToBoot[Dependacy.FileName]
+                if FileInitObjs then
+                    for i=1, #FileInitObjs do
+                        local PotentialInitObj = FileInitObjs[i]
+                        if PotentialInitObj.Version == Dependacy.Version then
+                            DependacyInitObj = PotentialInitObj
+                        end
+                    end
+                    
+                end
+            end
             local DependacyCluster = Dependacies[DependacyKey]
             if not DependacyCluster then
                 DependacyCluster = {
                     Object = Dependacy;
+                    InitObj = DependacyInitObj;
                     Dependants = {}
                 }
                 Dependacies[DependacyKey] = DependacyCluster
@@ -194,36 +208,49 @@ function InitializerService:CheckDependacies(_Globals: table?)
                 table.insert(DependacyCluster,DependacyVersion)]]
             end
             table.insert(DependacyCluster.Dependants,FileToBoot)
+            table.insert(DependacyOrder,DependacyKey)
         end
-        self:AddToBootGroup(FileToBoot)
+        --self:AddToBootGroup(FileToBoot)
     end
-    for DependacyNameVersion, DependacyInfo in next, Dependacies do
+    --order the DependacyOrder
+    table.sort(DependacyOrder, function(Dependacy1:FileNameVersion,Dependacy2:FileNameVersion)
+        local Dependacy1Obj = Dependacies[Dependacy1].InitObj
+        local Dependacy2Obj = Dependacies[Dependacy2].InitObj
+        if Dependacy1Obj ~= nil then
+            if Dependacy2Obj ~= nil then
+                return Dependacy1Obj.BootOrder < Dependacy2Obj.BootOrder
+            else
+                return true
+            end
+        else
+            return false
+        end
+    end)
+    for i=1, #DependacyOrder do
+        local DependacyNameVersion = DependacyOrder[i]
+        local DependacyInfo = Dependacies[DependacyNameVersion]
         print("Getting Dependacy:",DependacyNameVersion)
         local DependacyName = DependacyInfo.Object.FileName
-        local PotentialDependacyFiles = FilesToBoot[DependacyName]
-        if PotentialDependacyFiles then
-            for i=1, #DependacyInfo do
-                local DependacyVersion = DependacyInfo[i]
-                local Success = false
-                print("Requested Dependacy Version:",DependacyVersion)
-                for j=1, #PotentialDependacyFiles do
-                    local ThisInitObject = PotentialDependacyFiles[j]
-                    print("ThisInitObject.Version:",ThisInitObject.Version)
-                    if ThisInitObject.Version == DependacyVersion then
-                        self:AddToBootGroup(ThisInitObject)
-                        Success = true
-                        break
-                    end
-                end
-                if Success == false then
-                    warn("Could not find",DependacyNameVersion,"! Potential errors will likely occur! Debug:",debug.traceback())
+        local DependacyInitObj = DependacyInfo.InitObj
+        print("DependacyInitObj:",DependacyInitObj)
+        if DependacyInitObj then --if the dependacy is in the same boot section, then make sure it is in a boot group that launches before all of the other dependants.
+            --local DependacyVersion = DependacyInfo.Object.Version
+            --local DependacyInitObj = DependacyInitObj[tostring(DependacyVersion)]
+            local EarliestBootOrder = DependacyInitObj.BootOrder
+            local Dependants = DependacyInfo.Dependants
+            for j=1, #Dependants do
+                local ThisDependant = Dependants[j]
+                local DependantBootOrder = ThisDependant.BootOrder
+                if EarliestBootOrder >= DependantBootOrder then
+                    EarliestBootOrder = DependantBootOrder - 1
                 end
             end
+            DependacyInitObj.BootOrder = EarliestBootOrder
         else
             local InitializedModule = self.InitializedModules[DependacyName]
             if InitializedModule then
-                for i=1, #DependacyInfo do
-                    local DependacyVersion = DependacyInfo[i]
+                for j=1, #DependacyInfo do
+                    local DependacyVersion = DependacyInfo[j]
                     if InitializedModule[tostring(DependacyVersion)] ~= nil then
                         print("Environment already has",DependacyNameVersion,"initialized. Next!")
                         continue
@@ -234,25 +261,7 @@ function InitializerService:CheckDependacies(_Globals: table?)
             end
         end
     end
-end
-
-function InitializerService:AddToBootGroup(SingletonInitObject: SingletonInitClass)
-    local BootGroups: BootGroups = self.BootGroups
-    local BootOrder = SingletonInitObject.BootOrder
-    local BootGroup = BootGroups[BootOrder]
-    if BootGroup == nil then
-        BootGroup = {} :: BootGroup --empty bootgroup
-    end
-    local CurrentInitName = SingletonInitObject.InitName
-    for i=1, #BootGroup do
-        local InitObject = BootGroup[i]
-        if InitObject.InitName == CurrentInitName then
-            --warn("Duplicate InitGroup of",CurrentInitName,"found! Not inserting duplicate! Debug:",debug.traceback())
-            return
-        end
-    end
-    table.insert(BootGroup,SingletonInitObject)
-    BootGroups[BootOrder] = BootGroup
+    
 end
 
 function InitializerService:Destroy()
@@ -264,14 +273,6 @@ function InitializerService:Destroy()
         self.FilesToBoot[FileName] = nil
     end
     self.FilesToBoot = nil
-    for BootOrderNumber, BootGroup in next, self.BootGroups do
-        for i=1, #BootGroup do
-            BootGroup[i] = nil
-        end
-        BootGroup = nil
-        self.BootGroups[BootOrderNumber] = nil
-    end
-    self.BootGroups = nil
     self = nil
 end
 
