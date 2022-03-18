4a0688f1cd2f441dab7681be7705d4cf108e45b3
Leo Yu Lee <leoyulee.585@gmail.com>
2022-03-17 20:47:09 -0500

    Suppressed Selene Lint

--- src/ServerStorage/ArcaneumEngine/Client/Scripts/PlayerInterface.client.lua
+++ src/ServerStorage/ArcaneumEngine/Client/Scripts/PlayerInterface.client.lua
@@ -3,7 +3,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Events = ReplicatedStorage:WaitForChild("Events")
 local PlayerInterface = Events:WaitForChild("PlayerInterface")
 local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
+local _LocalPlayer = Players.LocalPlayer
 PlayerInterface.OnClientEvent:Connect(function(Data)
     print("Got Data:", Data)
 end)
