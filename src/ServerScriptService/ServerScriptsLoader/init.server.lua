b8c26d489a3b4a046130c4af640b5402face376d
leoyulee <leoyulee.585@gmail.com>
2022-04-04 20:07:05 -0500

    Removed debrecated code

--- src/ServerScriptService/ServerScriptsLoader/init.server.lua
+++ src/ServerScriptService/ServerScriptsLoader/init.server.lua
@@ -9,29 +9,10 @@ print("Booting")
 local Software = require(BootDrive)
 print("Booted")
 
-local Players = game:GetService("Players")
 local ArcaneumGlobals = Software.Globals
---[[
-local RunService = game:GetService("RunService")
-print(RunService:IsRunMode())
-if ArcaneumGlobals.IsStudio and not RunService:IsRunMode() then
-    game:GetService("Players").PlayerAdded:Wait()]]
---end
-local WaitTime = 0
 local TestModuleName = "ArcaneumTests"
 local TestModule = ServerStorage:FindFirstChild(TestModuleName)
 if ArcaneumGlobals.IsTesting then
-    print("Waiting a little in case a player is being added...\n Starting tests in:")
-    for i=WaitTime, 1, -1 do
-        print(i)
-        task.wait(1)
-    end
-    local TestPlayer do
-        local AllPlayers = Players:GetPlayers()
-        if #AllPlayers > 0 then
-            TestPlayer = AllPlayers[1]
-        end
-    end
-    local TestService = require(TestModule):New(TestPlayer)
+    local TestService = require(TestModule):New()
     TestService:Run()
 end
