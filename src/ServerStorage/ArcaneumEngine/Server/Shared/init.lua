5a477999b6d1c0a6a771eff34e98fb5ee873ebcf
leoyulee <leoyulee.585@gmail.com>
2022-04-07 05:55:30 -0500

    Removed InitializerService

--- src/ServerStorage/ArcaneumEngine/Server/Shared/init.lua
+++ src/ServerStorage/ArcaneumEngine/Server/Shared/init.lua
@@ -1,16 +1,5 @@
-local ModuleInfo =  {
-    InitName = script.Name;
-    BootOrder = 1;
-    Version = "1.0.0";
-    Dependacies = {
-        Utilities = "1.0.0"
-    };
-}
-local ServerGlobals = {}
-function ServerGlobals.Setup(_output: table, ArcaneumGlobals: table): table
-    local Utilities = ArcaneumGlobals.Utilities
-    ServerGlobals = Utilities:ModulesToTable(script:GetChildren())
-    return ServerGlobals
-end;
-ModuleInfo.__call = ServerGlobals.Setup
-return ModuleInfo
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ArcaneumGlobals = require(ReplicatedStorage.Arcaneum)
+local Utilities = ArcaneumGlobals.Utilities
+local ServerGlobals = Utilities:ModulesToTable(script:GetChildren())
+return ServerGlobals
