f15d989234bbf03fe6be5ec7508d9bc602e0a9fd
leoyulee <leoyulee.585@gmail.com>
2022-04-08 04:47:11 -0500

    Added docs and changed AddClass process

--- src/ServerStorage/ArcaneumEngine/Global/ClassFunctions/init.lua
+++ src/ServerStorage/ArcaneumEngine/Global/ClassFunctions/init.lua
@@ -1,20 +1,60 @@
 local BaseClass = require(script.BaseClass)
 BaseClass:CheckVersion("1.0.0")
-local ClassService = BaseClass:New("ClassService","1.0.0")
-function ClassService:AddClass(ClassName: string, ClassData: table): any
-    self[ClassName] = ClassData
+--[=[
+    @class ClassService
+    A singleton that allows quick indexing of initialized classes in the environment.
+]=]
+local ClassService: ClassService = BaseClass:Extend({
+    ClassName = "ClassService";
+    Version = "1.0.0";
+    Classes = {};
+})
+export type ClassService = {
+    Classes: {[string]: any};
+} & typeof(ClassService) & BaseClass.BaseClass
+
+--[=[
+    Adds the class into the Classes cache so it can be retrieved via [ClassSerivce:GetClass].
+
+    @param ClassName string -- The name of the class being added.
+    @param ClassData any -- The data of the class being added.
+    @return any -- The class itself.
+]=]
+function ClassService:AddClass<Data>(ClassName: string, ClassData: Data): Data
+    if self.Classes[ClassName] ~= nil then
+        warn("Overwritting " .. ClassName .."! Unintended bugs might occur! Debug:\n"..debug.traceback())
+    end
+    self.Classes[ClassName] = ClassData
     return ClassData
 end
---ClassService:AddClass("BaseClass",BaseClass)
-function ClassService:GetClass(ClassName: string): any
-    local RequestedClass = self[ClassName]
+
+--[=[
+    A wrapper for [ClassService:AddClass] where you pass in a ModuleScript with the class data instead.
+
+    @param ClassModule ModuleScript -- The ModuleScript with the name of the class and contains the ClassData when required.
+    @return any -- The class itself.
+]=]
+function ClassService:AddClassModule(ClassModule: ModuleScript): any
+    assert(typeof(ClassModule) == "Instance", "ClassModule is not an Instance! Debug:\n"..debug.traceback())
+    assert(ClassModule:IsA("ModuleScript"), "ClassModule is not a ModuleScript! Debug:\n"..debug.traceback())
+    return self:AddClass(ClassModule.Name, require(ClassModule))
+end
+
+--[=[
+    Returns the requested class, if it exists.
+
+    @param ClassName string -- The name of the requested class.
+    @return any -- The class itself, if it exists.
+]=]
+function ClassService:GetClass(ClassName: string): any?
+    local RequestedClass = self.Classes[ClassName]
     if RequestedClass ~= nil then
         return RequestedClass
     else
         warn("ClassService was asked to return a class that doesn't exist!",ClassName,debug.traceback())
     end
 end
-local function UnpackClasses(Parent: ModuleScript): table
+local function UnpackClasses(Parent: ModuleScript)
     local PotentialModules = Parent:GetChildren()
     for i=1, #PotentialModules do
         local PotentialModule = PotentialModules[i]
