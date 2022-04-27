525d2c3069229b661599dbafab5db996ce64bc18
Leo <56608247+leoyulee@users.noreply.github.com>
2022-04-27 00:53:42 -0500

    Added SetClassParent functionality

--- src/ServerStorage/ArcaneumEngine/Global/ClassService/init.lua
+++ src/ServerStorage/ArcaneumEngine/Global/ClassService/init.lua
@@ -6,7 +6,7 @@ BaseClass:CheckVersion("1.2.0")
 ]=]
 local ClassService: ClassService = BaseClass:Extend({
     ClassName = "ClassService";
-    Version = "1.0.0";
+    Version = "1.1.0";
     CoreModule = script;
     Classes = {};
     AddOnFolder = script.AddOns;
@@ -17,6 +17,7 @@ export type ClassService = {
 } & typeof(ClassService) & BaseClass.BaseClass
 
 --[=[
+    @since 1.0.0
     Adds the class into the Classes cache so it can be retrieved via [ClassSerivce:GetClass].
 
     @param ClassName string -- The name of the class being added.
@@ -32,31 +33,70 @@ function ClassService:SetClass<Data>(ClassName: string, ClassData: Data): Data
 end
 
 --[=[
+    @since 1.1.0
+    Sets the ClassModule's parent to be the ParentClass's modulescript.
+    @param ClassModule ModuleScript -- The ModuleScript to be moved.
+    @param ParentClass string -- The class which the ClassModule should be parented under. If "AddOn" is put in, it will instead be put into the AddOns folder.
+
+    @error "Attempted to add ClassModule to a class ([ParentClass]) that hasn't been made yet!" -- Occurs when the ParentClass doesn't exist in the service.
+    @error "[ParentClass].CoreModule is nil!" -- Occurs when the ParentClass doesn't have a CoreModule property.
+]=]
+function ClassService:SetClassParent(ClassModule: ModuleScript, ParentClass: string)
+    if ParentClass ~= "AddOn" then
+        local TargetParent = self:GetClass(ParentClass)
+        if TargetParent and TargetParent.CoreModule then
+            if TargetParent.CoreModule then
+                ClassModule.Parent = TargetParent.CoreModule
+            else
+                warn(debug.traceback(ParentClass..".CoreModule is nil!"))
+            end
+        else
+            warn(debug.traceback("Attempted to add ClassModule to a class ("..ParentClass..") that hasn't been made yet!"))
+        end
+    else
+        ClassModule.Parent = self.AddOnFolder
+    end
+end
+
+--[=[
+    @since 1.1.0
     A wrapper for [ClassService:AddClass] where you pass in a ModuleScript with the class data instead.
 
     @param ClassModule ModuleScript -- The ModuleScript with the name of the class and contains the ClassData when required.
-    @return any -- The class itself.
+    @param ParentClass string? -- The class which the ClassModule should be parented under. If "AddOn" is put in, it will instead be put into the AddOns folder.
+    @return table -- The class itself.
 ]=]
-function ClassService:AddClassModule(ClassModule: ModuleScript): any
+function ClassService:AddClassModule(ClassModule: ModuleScript, ParentClass: string?): table
     assert(typeof(ClassModule) == "Instance", "ClassModule is not an Instance! Debug:\n"..debug.traceback())
     assert(ClassModule:IsA("ModuleScript"), "ClassModule is not a ModuleScript! Debug:\n"..debug.traceback())
+    if ParentClass ~= nil then
+        self:SetClassParent(ClassModule, ParentClass)
+    end
     return self:SetClass(ClassModule.Name, require(ClassModule))
 end
 
 --[=[
+    @since 1.0.0
     Returns the requested class, if it exists.
 
     @param ClassName string -- The name of the requested class.
     @return any -- The class itself, if it exists.
 ]=]
 function ClassService:GetClass(ClassName: string): any?
+    assert(type(ClassName) == "string", debug.traceback("Invalid ClassName given!"))
     local RequestedClass = self.Classes[ClassName]
     if RequestedClass ~= nil then
         return RequestedClass
     else
-        warn("ClassService was asked to return a class that doesn't exist!",ClassName,debug.traceback())
+        warn(debug.traceback("ClassService was asked to return a class that doesn't exist! Requested class: "..ClassName))
     end
 end
+
+--[=[
+    @since 1.0.0
+    @private
+    Sets up the initial classes upon being required.
+]=]
 local function UnpackClasses(Parent: ModuleScript)
     local PotentialModules = Parent:GetChildren()
     for i=1, #PotentialModules do
