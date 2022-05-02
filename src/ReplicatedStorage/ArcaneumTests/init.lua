e8d74f501004769c0c08fd85165494366ff81951
leoyulee <leoyulee.585@gmail.com>
2022-05-02 03:14:20 -0500

    Refactored for GlobalTests

--- src/ReplicatedStorage/ArcaneumTests/init.lua
+++ src/ReplicatedStorage/ArcaneumTests/init.lua
@@ -1,6 +1,5 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local GlobalModuleName = "Arcaneum"
-local Separator = "----------------------------------"
 local ArcaneumGlobals repeat
     ArcaneumGlobals = ReplicatedStorage:FindFirstChild(GlobalModuleName)
     if ArcaneumGlobals == nil then
@@ -16,116 +15,51 @@ local Class = ClassService:GetClass("Class")
 Class:CheckVersion("1.2.0")
 local Utilities = ArcaneumGlobals:GetGlobal("Utilities")
 Utilities:CheckVersion("1.0.0")
-local TesterClass = require(script.Tester)
-local TestsClass = require(script.TestInfoInterface)
+local TestCollectionClass = require(script.TestCollection)
 local TestBot: TestBot = Class:Extend(
     {
         ClassName = "ArcaneumTestService";
         ArcaneumGlobals = ArcaneumGlobals;
         Version = "1.0.0";
         CoreModule = script;
-        TesterClass = TesterClass;
-        TestModules = {};
-        TestData = {
-            Tests = {};
-            Positions = {};
-        }
+        TestCollections = {};
     }
 )
 export type TestBot = {
-    TesterClass: TesterClass.Tester;
-    TestModules: Array<ModuleScript>;
-    TestData: {
-        Tests: Array<TestsClass.TestInfo>;
-        Positions: Array<number>;
-    };
-}
+    TestCollections: Dictionary<TestCollectionClass.TestCollection>;
+} & typeof(TestBot) & typeof(Class)
 function TestBot:New(Tests: Folder): TestBot
+    local NewBot = self:Extend({});
     assert(Tests ~= nil, "No tests provided! Debug:\n"..debug.traceback())
-    local TestModules = {}
+    local TestCollections = {}
+    TestCollections.GlobalTests = TestCollectionClass:New("GlobalTests",script.GlobalTests);
     local TestModulesFolder = Tests do
         if TestModulesFolder then
-            TestModules = TestModulesFolder:GetChildren()
+            TestCollections.OtherTests = TestCollectionClass:New(Tests.Name,Tests);
         end
     end
-    self.TestModules = TestModules
-    local NumberOfTests = #TestModules
-    print("Number of Testers to check:",NumberOfTests)
-    local TestData = {};
-    for i=1, #TestModules do
-        local ModuleScript = TestModules[i]
-        if ModuleScript:IsA("ModuleScript") then
-            print("Initializing test:",ModuleScript)
-            local TestInfo: TestsClass.TestInfo = require(ModuleScript)
-            table.insert(TestData, TestInfo)
-        end
-    end
-    table.sort(TestData,function(a,b)
-        local aSuc, aNum = pcall(function()
-            return a.TestPriority + 0
-        end)
-        local bSuc, bNum = pcall(function()
-            return b.TestPriority + 0
-        end)
-        if aSuc and bSuc then
-            return aNum < bNum
-        elseif aSuc then
-            return true
-        elseif bSuc then
-            return false
-        end
-        assert(aSuc and bSuc,"One of these aren't a number! A: " .. a.Name .. ", B: " .. b.Name)
-    end)
-    TestBot.TestData = TestData
-    return self
+    NewBot.TestCollections = TestCollections
+    return NewBot
 end
 
 function TestBot:Run()
-    local TestData = self.TestData
-    print("Running:",TestData)
-    local FailedCounter, WarnCounter, SkippedCounter = 0,0,0
-    for i=1, #TestData do
-        local TesterInitData = TestData[i]
-        if TesterInitData.ToRun == true then
-            local Tester = self.TesterClass:New(TesterInitData.TestName)
-            Tester:SetPrintProcess(TesterInitData.ToPrintProcess)
-            Tester = TesterInitData.Init(self, Tester)::TesterClass.Tester
-            if Tester == 3 then
-                SkippedCounter += 1
-                continue
-            end
-            if Tester.RunTests then
-                local DisplayName = Tester.DisplayName
-                local TestName = Tester.Name
-                print("\n\n"..Separator.."\n" .. DisplayName .. " will now start their tests (".. TestName ..").")
-                local TesterFeedback = Tester:RunTests()
-                print("\n\n"..DisplayName.." has finished their tests! Here's their report:")
-                for j=1, #TesterFeedback do
-                    local Result = TesterFeedback[j]
-                    if Result.Status == "Successful" then
-                        print(Result)
-                    elseif Result.Status == "Skipped" then
-                        SkippedCounter += 1
-                        warn("Test was skipped!")
-                    elseif Result.Status == "Failure" then
-                        warn(Result)
-                        WarnCounter += 1
-                    elseif Result.Status == "Critical Failure" then
-                        Utilities:error(Result)
-                        FailedCounter += 1
-                    else
-                        warn("Test did not have a Status!")
-                    end
-                end
-                print(Separator .."\n\n")
-            end
-        else
-            SkippedCounter += 1
-            continue
-        end
+    local TestCollections = self.TestCollections
+    local FailedCounter, WarnCounter, SkippedCounter = TestCollections.GlobalTests:Run()
+    if TestCollections.OtherTests ~= nil then
+        local OtherFailed, OtherWarn, OtherSkipped = TestCollections.OtherTests:Run()
+        FailedCounter += OtherFailed
+        WarnCounter += OtherWarn
+        SkippedCounter += OtherSkipped
     end
     print(FailedCounter,"failed,", WarnCounter, "issue(s),", SkippedCounter, "skipped")
-    return true
+    self:Destroy()
+end
+
+function TestBot:Destroy()
+    for _,v in pairs(self.TestCollections) do
+        v:Destroy()
+    end
+    return Class.Destroy(self)
 end
 
 return TestBot
